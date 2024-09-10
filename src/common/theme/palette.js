c7a8a133b76bb5fa7cc94b6948fad08d945291bd
Dax <daxnavarrete@gmail.com>
2024-09-10 14:39:16 -0500

    Change style features-title, color palette and logo

--- src/common/theme/palette.js
+++ src/common/theme/palette.js
@@ -1,22 +1,27 @@
-import { grey, green, indigo } from '@mui/material/colors';
+import { grey, green, teal } from "@mui/material/colors";
 
-const validatedColor = (color) => (/^#([0-9A-Fa-f]{3}){1,2}$/.test(color) ? color : null);
+const validatedColor = (color) =>
+  /^#([0-9A-Fa-f]{3}){1,2}$/.test(color) ? color : null;
 
 export default (server, darkMode) => ({
-  mode: darkMode ? 'dark' : 'light',
+  mode: darkMode ? "dark" : "light",
   background: {
     default: darkMode ? grey[900] : grey[50],
   },
   primary: {
-    main: validatedColor(server?.attributes?.colorPrimary) || (darkMode ? indigo[200] : indigo[900]),
+    main:
+      validatedColor(server?.attributes?.colorPrimary) ||
+      (darkMode ? teal[300] : teal[700]),
   },
   secondary: {
-    main: validatedColor(server?.attributes?.colorSecondary) || (darkMode ? green[200] : green[800]),
+    main:
+      validatedColor(server?.attributes?.colorSecondary) ||
+      (darkMode ? green[300] : green[600]),
   },
   neutral: {
     main: grey[500],
   },
   geometry: {
-    main: '#3bb2d0',
+    main: "#3bb2d0",
   },
 });
